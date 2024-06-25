d94ea7b69d86e6b4d66aae5469408e74216a4f34
Topi Mansikkaviita <AC8407@student.labranet.jamk.fi>
2024-06-25 19:44:48 +0300

    Tehtävä 1.5, koko olion asettaminen propsina

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -3,55 +3,65 @@ const Header = (props) =>
 {
     return (
             <>
-                <h1>{props.course}</h1> 
+                <h1>{props.course.name}</h1> 
             </>
             );
 }
 
 
-const Part = (props) =>
+const Content = (props) => 
 {
-    return  (    
-            <>
-                <p>{props.data.name} {props.data.exercises}</p>
-            </>
-            )
+    return  (
+                <>
+                    {props.course.parts.map( (part, index) => 
+                        (
+                            <p key={index}>{part.name} {part.exercises}</p>
+                        ))
+                    }
+                </>
+            );        
 }
 
 const Total = (props) => 
 {
+    let sum = 0;
+    for(let part of props.course.parts)
+    {
+        sum += part.exercises;
+    }
     return  (    
             <>
-                <p>Number of exercises {props.exercises}</p>
+                <p>Number of exercises {sum}</p>
             </>
             )
 }
 
 const App = () => 
 {
-    const parts = 
-    [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
-
+    const course = 
+    {
+        name: 'Half Stack application development',
+        parts: 
+        [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
     return (
       <div>
-        <Header course="Half Stack application development"/>
-        <Part data={parts[0]}/>
-        <Part data={parts[1]}/>
-        <Part data={parts[2]}/>
-        <Total exercises={parts[0].exercises+parts[1].exercises+parts[2].exercises}/>
+        <Header course={course}/>
+        <Content course={course}/>
+        <Total course={course}/>
       </div>
     )
 }
