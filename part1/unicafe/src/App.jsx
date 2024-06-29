07f48a27f05ec886d91a4a655471f21a286dafeb
Topi Mansikkaviita <AC8407@student.labranet.jamk.fi>
2024-06-29 15:14:19 +0300

    Unicafe, step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css';
+import { useState } from 'react'
+
+const Button = (props) =>
+{
+    return  (
+                <button onClick={props.handleClick}>{props.text}</button>
+            )
+}
+
+const Statistics = (props) =>
+{
+    return  (
+                <>
+                    <h1>statistics</h1>
+                    <ul>
+                        <li>good {props.data[0]}</li>
+                        <li>neutral {props.data[1]}</li>
+                        <li>bad {props.data[2]}</li>
+                    </ul>
+                </>
+            )
+}
+ 
+const App = () => 
+{
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleClick = (args) => 
+    {
+        const handler = () => 
+        {      
+            switch(args)
+            {
+                case "good": setGood(good + 1); break;
+                case "neutral": setNeutral(neutral + 1); break;
+                case "bad": setBad(bad + 1); break;
+            }
+        }
+        return handler
+    }
+
+    return  (
+                <div>
+                    <h1>give feedback</h1>
+                    <Button handleClick={handleClick("good")} text='good'/>
+                    <Button handleClick={handleClick("neutral")} text='neutral'/>
+                    <Button handleClick={handleClick("bad")} text='bad'/>
+                    <Statistics data={[good, neutral, bad]}/>
+                </div>
+            )
+}
+
+export default App
