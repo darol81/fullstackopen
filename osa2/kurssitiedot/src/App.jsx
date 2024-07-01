e72d1c28abbda192e5f69c8cf2674dabbfcb1266
Topi Mansikkaviita <AC8407@student.labranet.jamk.fi>
2024-07-01 17:23:24 +0300

    Kurssitiedot, step9

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -41,32 +41,68 @@ const Course = ({ course }) =>
                     <Total course={course}/>
                 </>
             )
-}   
+}  
+
+const Courses = ({courses}) => 
+{
+    return  (
+                <>
+                    {courses.map(course => <Course key={course.id} course={course}/>)}
+                </>
+            )
+}
 
 const App = () => 
 {
-    const course = 
-    {
-        name: 'Half Stack application development',
-        parts: 
-        [
+    const courses = 
+    [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: 
+            [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        }, 
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
             {
-                name: 'Fundamentals of React',
-                exercises: 10
+                name: 'Routing',
+                exercises: 3,
+                id: 1
             },
             {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
+                name: 'Middlewares',
+                exercises: 7,
+                id: 2
             }
-        ]
-    }
+            ]
+        }
+    ]
     return (
       <div>
-        <Course course={course} />
+        <Courses courses={courses} />
       </div>
     )
 }
