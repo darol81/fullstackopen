2a01405f0ca5522b955d6d1368dd933ea2451762
Topi Mansikkaviita <tomansi@gmail.com>
2024-07-17 21:12:26 +0300

    Notes-sovellus, osa4, omat moduulit kaikelle

---
+++ osa4/notes-backend/app.js
@@ -0,0 +1,45 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors")
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+
+/* Route controllers */
+
+const notesRouter = require("./controllers/notes");
+
+/* Database Connection with Mongoose */
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+logger.info("Connecting to", config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI).then( () =>
+{
+	logger.info("Connected to MongoDB.");
+}).catch( (error) =>
+{
+	logger.error("Error connecting to MongoDB:", error.message);
+});
+
+
+/* Middlewares */
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
+
+/* Morgan middleware */
+morgan.token("body", request => JSON.stringify(request.body));
+const tiny_format = ":method :url :status :res[content-length] - :response-time ms :body";
+app.use(morgan(tiny_format));
+
+/* Routes */
+
+app.use("/api/notes", notesRouter);
+
+/* Own middleware */
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ osa4/notes-backend/controllers/notes.js
@@ -0,0 +1,78 @@
+const notesRouter = require("express").Router();
+
+/* Models */
+const Note = require("../models/note");
+
+/* POST routes */
+notesRouter.post("/", (request, response, next) =>
+{
+	const body = request.body;
+
+	if (!body.content)
+	{
+		return response.status(400).json({ error: "content missing" });
+	}
+
+	const note = new Note
+    ({
+		content: body.content,
+		important: body.important || false,
+	});
+
+	note.save().then(savedNote =>
+	{
+		response.json(savedNote);
+	}).catch(error => next(error));
+});
+
+/* PUT routes */
+notesRouter.put("/:id", (request, response, next) =>
+{
+	const body = request.body;
+
+	const note = 
+    {
+		content: body.content,
+		important: body.important || false
+	};
+
+	Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: "query" }).then(updatedNote =>
+	{
+		response.json(updatedNote);
+	}).catch(error => next(error));
+});
+
+/* GET routes */
+notesRouter.get("/", (request, response) =>
+{
+	Note.find({}).then(notes =>
+	{
+		response.json(notes);
+	});
+});
+
+notesRouter.get("/:id", (request, response, next) =>
+{
+	Note.findById(request.params.id).then(note =>
+	{
+		if(note)
+		{
+			response.json(note);
+		}
+		else
+		{
+			response.status(404).end();
+		}
+	}).catch(error => next(error));
+});
+
+/* DELETE routes */
+notesRouter.delete("/:id", (request, response, next) =>
+{
+	Note.findByIdAndDelete(request.params.id).then(() =>
+	{
+		response.status(204).end();
+	}).catch(error => next(error));
+});
+
+module.exports = notesRouter;
