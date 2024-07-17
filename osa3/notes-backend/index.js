6e127f8da4710497bd2d8c064cacd7b64648249a
Topi Mansikkaviita <tomansi@gmail.com>
2024-07-17 19:39:50 +0300

    Päivitystä osaa 4 varten. Linttaukset/mongodb/kotitietokoneen asetukset

--- osa3/notes-backend/index.js
+++ osa3/notes-backend/index.js
@@ -1,96 +1,128 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+
 /* Middlewares */
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
-/*const requestLogger = (request, response, next) => 
-{
-    console.log("Method:", request.method);
-    console.log("Path:  ", request.path);
-    console.log("Body:  ", request.body);
-    console.log("---");
-    next();
-}
-*/
-//app.use(requestLogger);
+/* Morgan middleware */
+morgan.token('body', request => JSON.stringify(request.body))
+const tiny_format = ':method :url :status :res[content-length] - :response-time ms :body'
+app.use(morgan(tiny_format))
 
-const Note = require("./models/note");
-
+/* Models */
+const Note = require('./models/note')
 
 /* ROUTES, main */
 
-app.get("/", (request, response) => 
+app.get('/', (request, response) =>
 {
-    response.send("<h1>Hello World!</h1>");
-});
+  response.send('<h1>Hello World!</h1>')
+})
 
 /* POST routes */
-app.post("/api/notes", (request, response) => 
-{  
-    const body = request.body;
+app.post('/api/notes', (request, response, next) =>
+{
+  const body = request.body
 
-    if (!body.content) 
-    {
-        return response.status(400).json({ error: "content missing" });
-    }
-  
-    const note = 
-    {
-        content: body.content,
-        important: body.important || false,
-    };
-  
-    notes = notes.concat(note);
-    response.json(note);
-});
+  if (!body.content)
+  {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote =>
+  {
+    response.json(savedNote)
+  }).catch(error => next(error))
+})
+
+/* PUT routes */
+app.put('/api/notes/:id', (request, response, next) =>
+{
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important || false
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' }).then(updatedNote =>
+  {
+    response.json(updatedNote)
+  }).catch(error => next(error))
+})
 
 /* GET routes */
-
-app.get("/api/notes", (request, response) => 
+app.get('/api/notes', (request, response) =>
 {
-    Note.find({}).then(notes => 
+  Note.find({}).then(notes =>
+  {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) =>
+{
+  Note.findById(request.params.id).then(note =>
+  {
+    if(note)
     {
-        response.json(notes)
-    });
-});
-
-app.get("/api/notes/:id", (request, response) => 
-{
-    const id = request.params.id;
-    const note = notes.find(note => note.id === id);
-
-    if(note) 
-    {    
-        response.json(note);
-    } 
-    else 
-    {    
-        response.status(404).end();
+      response.json(note)
     }
-});
+    else
+    {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
+})
 
-/* ROUTES, delete */
-app.delete("/api/notes/:id", (request, response) => 
+/* DELETE routes */
+app.delete('/api/notes/:id', (request, response, next) =>
 {
-    const id = request.params.id;
-    notes = notes.filter(note => note.id !== id);
-    response.status(204).end();
-});
+  Note.findByIdAndDelete(request.params.id).then(() =>
+  {
+    response.status(204).end()
+  }).catch(error => next(error))
+})
 
-const unknownEndpoint = (request, response) => 
+/* Unknown endpoint */
+const unknownEndpoint = (request, response) =>
 {
-    response.status(404).send({ error: "unknown endpoint" });
+  response.status(404).send({ error: 'unknown endpoint' })
 }
- 
-app.use(unknownEndpoint);
+
+app.use(unknownEndpoint)
+
+/* Error handling */
+const errorHandler = (error, request, response, next) =>
+{
+  console.error(error.message)
+
+  if(error.name === 'CastError')
+  {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if(error.name === 'ValidationError')
+  {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 /* Pääohjelma */
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
 {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- osa3/notes-backend/models/note.js
+++ osa3/notes-backend/models/note.js
@@ -1,34 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 /* Mongoose */
-const url = process.env.MONGODB_URI;
-mongoose.set("strictQuery", false);
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery', false)
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
-mongoose.connect(url).then(result => 
-{    
-    console.log("Connected to MongoDB");
-}).catch((error) => 
-{   
-    console.log("error connecting to MongoDB:", error.message);  
-});
+mongoose.connect(url).then( () =>
+{
+  console.log('Connected to MongoDB.')
+
+}).catch( (error) =>
+{
+  console.log('Error connecting to MongoDB:', error.message)
+})
 
 const noteSchema = new mongoose.Schema
 ({
-    content: String,
-    important: Boolean,
-});
+  content: String,
+  important: Boolean,
+})
 
-noteSchema.set('toJSON', 
-{
-    transform: (document, returnedObject) => 
+noteSchema.set('toJSON',
+  {
+    transform: (document, returnedObject) =>
     {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
     }
-});
+  })
 
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
