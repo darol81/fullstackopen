c6e0b1ae794a19aa1601549246e2d99a712006fe
Topi Mansikkaviita <tomansi@gmail.com>
2024-07-19 17:50:53 +0300

    tehtävä 4.3, apufunktioita ja yksikkötestejä, step 1

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,28 +1,28 @@
-const blogsRouter = require('express').Router()
+const blogsRouter = require("express").Router();
 
 /* Models */
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
 
 /* POST routes */
 
-blogsRouter.post('/', (request, response, next) =>
+blogsRouter.post("/", (request, response, next) =>
 {
-  const blog = new Blog(request.body)
+	const blog = new Blog(request.body);
 
-  blog.save().then(result =>
-  {
-    response.status(201).json(result)
-  }).catch(error => next(error))
-})
+	blog.save().then(result =>
+	{
+		response.status(201).json(result);
+	}).catch(error => next(error));
+});
 
 /* GET routes */
 
-blogsRouter.get('', (request, response, next) =>
+blogsRouter.get("", (request, response, next) =>
 {
-  Blog.find({}).then(blogs =>
-  {
-    response.json(blogs)
-  }).catch(error => next(error))
-})
+	Blog.find({}).then(blogs =>
+	{
+		response.json(blogs);
+	}).catch(error => next(error));
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
