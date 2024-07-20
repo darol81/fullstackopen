4484729abe9a8150794eda460c6dd85c57d8e773
Topi Mansikkaviita <tomansi@gmail.com>
2024-07-20 14:05:29 +0300

    Palautetaan mongo.js, kadonnut jonnekin

---
+++ osa4/notes-backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) 
+{
+	console.log("give password as argument");
+	process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.kcfxst6.mongodb.net/TestNotesApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url).then(() => 
+{
+	const noteSchema = new mongoose.Schema
+	({
+		content: String,
+		important: Boolean,
+	});
+
+	const Note = mongoose.model("Note", noteSchema);
+
+
+  const note = new Note
+  ({
+    content: 'Testitapaus 3',
+    important: true,
+  })
+
+  note.save().then(result => 
+{
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  
+	Note.find({}).then(result => 
+	{
+		result.forEach(note => 
+		{
+			console.log(note);
+		});
+		mongoose.connection.close();
+	});
+});
