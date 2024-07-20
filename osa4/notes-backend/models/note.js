44ee62a4f026c470903d08777549a7f5e5f95743
Topi Mansikkaviita <tomansi@gmail.com>
2024-07-20 15:19:06 +0300

    Korjattu sisennyksiä

--- osa4/notes-backend/models/note.js
+++ osa4/notes-backend/models/note.js
@@ -12,14 +12,14 @@ const noteSchema = new mongoose.Schema
 })
 
 noteSchema.set('toJSON',
-  {
+{
     transform: (document, returnedObject) =>
     {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 
 module.exports = mongoose.model('Note', noteSchema)
