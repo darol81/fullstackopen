42a39da91b72de87bac515b48cb5c4de2dfdd0e9
Topi Mansikkaviita <tomansi@gmail.com>
2024-07-29 00:17:03 +0300

    blogilistan laajennus, 4.23, step 11, testien korjaus

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,99 +1,99 @@
-const blogsRouter = require("express").Router();
+const blogsRouter = require('express').Router()
 
 /* Models */
-const Blog = require("../models/blog");
-const User = require("../models/user");
+const Blog = require('../models/blog')
+const User = require('../models/user')
 
 /* POST routes */
-blogsRouter.post("/", async(request, response) =>
+blogsRouter.post('/', async(request, response) =>
 {
-	const { title, author, url, likes } = request.body;
-    const userId = request.user;
+  const { title, author, url, likes } = request.body
+  const userId = request.user
 
-    if(!userId)
-	{    
-		return response.status(401).json({ error: "token invalid" });  
-	}
-    const user = await User.findById(userId);
+  if(!userId)
+  {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(userId)
  	const blog = new Blog
-	({
-		title,
-		author,
-		url,
-		likes: likes || 0,
-        user: user._id
-	});
+  ({
+    title,
+    author,
+    url,
+    likes: likes || 0,
+    user: user._id
+  })
 
-	const result = await blog.save();
-    user.blogs = user.blogs.concat(result._id);
-    await user.save();
-	response.status(201).json(result);
-});
+  const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+  response.status(201).json(result)
+})
 
 /* GET routes */
 
-blogsRouter.get("/", async(request, response) =>
+blogsRouter.get('/', async(request, response) =>
 {
-    const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 , id: 1 });
-	response.json(blogs);
-});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 , id: 1 })
+  response.json(blogs)
+})
 
-blogsRouter.get("/:id", async (request, response) =>
+blogsRouter.get('/:id', async (request, response) =>
 {
-	const blog = await Blog.findById(request.params.id);
-	if(blog)
-	{
-		response.json(blog);
-	}
-	else
-	{
-		response.status(404).end();
-	}
-});
+  const blog = await Blog.findById(request.params.id)
+  if(blog)
+  {
+    response.json(blog)
+  }
+  else
+  {
+    response.status(404).end()
+  }
+})
 
 /* PUT routes */
-blogsRouter.put("/:id", async (request, response) =>
+blogsRouter.put('/:id', async (request, response) =>
 {
-	const body = request.body;
+  const body = request.body
 
-	const content = 
+  const content =
     {
     	title: body.title,
     	author: body.author,
     	url: body.url,
     	likes: body.likes,
-    };
-	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, content,
+    }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, content,
     {
-        new: true,
-        runValidators: true,
-        context: "query"
-    });
-	response.json(updatedBlog);
-});
+      new: true,
+      runValidators: true,
+      context: 'query'
+    })
+  response.json(updatedBlog)
+})
 
 /* DELETE routes */
 
-blogsRouter.delete("/:id", async (request, response) =>
+blogsRouter.delete('/:id', async (request, response) =>
 {
-    const blog = await Blog.findById(request.params.id);
-    if (!blog) 
-    {
-        return response.status(404).json({ error: "Blog not found" });
-    }
-    const user = request.user;
-	if(!user)
-	{    
-		return response.status(401).json({ error: "token invalid" });  
-	}
-    /* Katsotaan, että blogin kirjoittaja ja poistaja on sama */
-    if(blog.user._id.toString() !== user)
-    {
-	    return response.status(401).json({ error: "unauthorized deletion" });  
-    }
-	await Blog.findByIdAndDelete(request.params.id);
-    await User.findByIdAndUpdate(blog.user._id, { $pull: { blogs: blog._id } });
-	response.status(204).end();
-});
+  const blog = await Blog.findById(request.params.id)
+  if (!blog)
+  {
+    return response.status(404).json({ error: 'Blog not found' })
+  }
+  const user = request.user
+  if(!user)
+  {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  /* Katsotaan, että blogin kirjoittaja ja poistaja on sama */
+  if(blog.user._id.toString() !== user)
+  {
+	    return response.status(401).json({ error: 'unauthorized deletion' })
+  }
+  await Blog.findByIdAndDelete(request.params.id)
+  await User.findByIdAndUpdate(blog.user._id, { $pull: { blogs: blog._id } })
+  response.status(204).end()
+})
 
-module.exports = blogsRouter;
+module.exports = blogsRouter
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,64 +1,64 @@
-const dummy = (blogs) => 
+const dummy = () =>
 {
-	return 1;
-};
-  
-const totalLikes = (blogs) => 
+  return 1
+}
+
+const totalLikes = (blogs) =>
 {
-	return blogs.reduce( (sum, blog) =>
-	{
-		return sum + blog.likes;
-	},  0);
-};
+  return blogs.reduce( (sum, blog) =>
+  {
+    return sum + blog.likes
+  },  0)
+}
 
 const favoriteBlog = (blogs) =>
 {
-	let most = -1;
-	let selectedBlog;
-	blogs.map(blog => 
-	{
-		if(selectedBlog == undefined || blog.likes > most)
-		{
-			selectedBlog = blog; 
-			most = blog.likes;
-		}
-	});
-	return selectedBlog;
-};
+  let most = -1
+  let selectedBlog
+  blogs.map(blog =>
+  {
+    if(selectedBlog === undefined || blog.likes > most)
+    {
+      selectedBlog = blog
+      most = blog.likes
+    }
+  })
+  return selectedBlog
+}
 
-const countMost = (blogs, callbackFunc, valueKey) => 
+const countMost = (blogs, callbackFunc, valueKey) =>
 {
-	let most = -1;
-	let author;
+  let most = -1
+  let author
 
-	let blogAmounts = { };
-	blogs.forEach( (blog) => 
-	{
-		if (!(blog.author in blogAmounts)) 
-		{
-			blogAmounts[blog.author] = 0;
-		}
-		blogAmounts[blog.author] += callbackFunc(blog);
-		if (!author || blogAmounts[blog.author] > most) 
-		{
-			most = blogAmounts[blog.author];
-			author = blog.author;
-		}
-	});
-	return { author: author, [valueKey]: most };
-};
+  let blogAmounts = { }
+  blogs.forEach( (blog) =>
+  {
+    if (!(blog.author in blogAmounts))
+    {
+      blogAmounts[blog.author] = 0
+    }
+    blogAmounts[blog.author] += callbackFunc(blog)
+    if (!author || blogAmounts[blog.author] > most)
+    {
+      most = blogAmounts[blog.author]
+      author = blog.author
+    }
+  })
+  return { author: author, [valueKey]: most }
+}
 
 const mostLikes = (blogs) =>
 {
-	return countMost(blogs, blog => blog.likes, "likes"); // haetaan blogin likemäärät
-};
+  return countMost(blogs, blog => blog.likes, 'likes') // haetaan blogin likemäärät
+}
 
 const mostBlogs = (blogs) =>
 {
-	return countMost(blogs, () => 1, "blogs"); // jokainen blogi palauttaa ykkösen, lasketaan blogien määrää foreachin sisällä.
-};
+  return countMost(blogs, () => 1, 'blogs') // jokainen blogi palauttaa ykkösen, lasketaan blogien määrää foreachin sisällä.
+}
 
-module.exports = 
+module.exports =
 {
-	dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
-};
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
