22301c4a47914ddc3e931546fb0d5b654beee440
Topi Mansikkaviita <tomansi@gmail.com>
2024-08-18 18:49:26 +0300

    5.7 blogilistan frontend, step 7

--- osa5/blogilista-frontend/src/components/Blog.jsx
+++ osa5/blogilista-frontend/src/components/Blog.jsx
@@ -1,8 +1,33 @@
-const Blog = ({ blog }) => 
-(
-    <div>
-        {blog.title} {blog.author}
-    </div>  
-);
+import { useState } from 'react'
 
-export default Blog
+const Blog = ({ blog }) => 
+{
+    const [inView, setInView] = useState(false);
+
+    const blogStyle = 
+    {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    return  (
+                <div>
+                    <div style={blogStyle}>
+                        {blog.title} {blog.author} <button onClick={() => setInView(!inView)}>{inView ? "Hide" : "View"}</button>
+                    {inView &&  (
+                                    <>
+                                        <br/>
+                                        {blog.url}<br/>
+                                        likes {blog.likes} <button>Like</button><br/>
+                                        {blog.user && blog.user.name}
+                                    </>
+                                )}            
+                    </div>
+                </div>
+            );
+}
+
+export default Blog;
