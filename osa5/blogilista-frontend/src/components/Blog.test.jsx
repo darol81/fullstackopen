ad470f4edcb5b4384f21572b26fa00535d8eca9b
Topi Mansikkaviita <tomansi@gmail.com>
2024-08-20 17:07:41 +0300

    5.15, blogilistan testit, step 3 (like button clicking)

--- osa5/blogilista-frontend/src/components/Blog.test.jsx
+++ osa5/blogilista-frontend/src/components/Blog.test.jsx
@@ -1,28 +1,37 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs';
+import { vi } from 'vitest';
+import { afterEach } from 'vitest';
+
+afterEach(() => 
+{
+    vi.clearAllMocks(); 
+});
 
 describe("<Blog/>:", () =>
 {
+    /* Samoja vakioita voi käyttää useammassakin testissä */
+    const userObj =
+    {
+        name: "Tom Tester",
+        token: "abc"
+    }
+    const blog = 
+    {
+        id: "abc",
+        title: "Testing with Vitest",
+        author: "Tom Tester",
+        url: "http://www.testingwithtom.com",
+        likes: 15,
+    };
+    const blogs = [];
+
     test("it renders blog and title without url or likes ", async() => 
     {
-        const user =
-        {
-            name: "Tom Tester",
-            token: "abc"
-        }
-        const blog = 
-        {
-            id: "abc",
-            title: "Testing with Vitest",
-            author: "Tom Tester",
-            url: "http://www.testingwithtom.com",
-            likes: 15,
-        };
-        const blogs = [];
-
         const mockHandler = vi.fn();
-        const { container } = render( <Blog key={blog.id} blog={blog} user={user} sortBlogs={mockHandler} blogs={blogs}/> );
+        const { container } = render( <Blog key={blog.id} blog={blog} user={userObj} sortBlogs={mockHandler} blogs={blogs}/> );
         const div = container.querySelector(".blog");
         expect(div).toHaveTextContent("Testing with Vitest Tom Tester");
         expect(div).not.toHaveTextContent("http://www.testingwithtom.com");
@@ -30,27 +39,6 @@ describe("<Blog/>:", () =>
     });
     test("It renders url, likes and author when View is clicked", async() => 
     {
-        const userObj =
-        {
-            name: "Tom Tester",
-            token: "abc"
-        }
-        const blog = 
-        {
-            id: "abc",
-            title: "Testing with Vitest",
-            author: "Tom Tester",
-            url: "http://www.testingwithtom.com",
-            likes: 15,
-            user: 
-            {   
-                username: "tom",
-                name: "Tom Tester",
-                id: "abc"
-            },
-        };
-        const blogs = [];
-
         const mockHandler = vi.fn();
         const { container } = render( <Blog key={blog.id} blog={blog} user={userObj} sortBlogs={mockHandler} blogs={blogs}/> );
         const div = container.querySelector(".blog");
@@ -61,6 +49,24 @@ describe("<Blog/>:", () =>
         expect(div).toHaveTextContent("http://www.testingwithtom.com");
         expect(div).toHaveTextContent("likes 15");
         expect(div).toHaveTextContent("Tom Tester");
+    });
+    test("handler gets called twice, when Like-button is clicked twice.", async() =>
+    {
+        vi.mock('../services/blogs'); // Mockataan blogservice
+        const mockHandler = vi.fn();
+        const updatedBlog = { ...blog, likes: blog.likes + 1 };
+        blogService.updateBlog.mockResolvedValue(updatedBlog); 
+
+        render( <Blog key={blog.id} blog={blog} user={userObj} sortBlogs={mockHandler} blogs={blogs}/> );
+        const user = userEvent.setup();
+        const viewButton = screen.getByText("View");
+        await user.click(viewButton);
+
+        const likeButton = screen.getByText("Like");
+        await user.click(likeButton);
+        await user.click(likeButton);
+        // Like-button kutsuu sortblogs, joka on mockattu mockHandlerilla.
+        expect(mockHandler).toHaveBeenCalledTimes(2);
     });
 });
 
