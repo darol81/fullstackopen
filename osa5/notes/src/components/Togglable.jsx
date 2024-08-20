0c8468d5aa66af55b795696f76672ab656bd3d3f
Topi Mansikkaviita <tomansi@gmail.com>
2024-08-20 14:22:51 +0300

    Notes-sovelluksen frontend-testejä Vitestillä

--- osa5/notes/src/components/Togglable.jsx
+++ osa5/notes/src/components/Togglable.jsx
@@ -1,42 +1,42 @@
 import { useState, useImperativeHandle, forwardRef } from 'react'
 import PropTypes from 'prop-types'
 
-const Togglable = forwardRef((props, ref) => 
+const Togglable = forwardRef((props, ref) =>
 {
-    const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false)
 
-    const hideWhenVisible = { display: visible ? 'none' : '' }
-    const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
 
-    const toggleVisibility = () => 
-    {
-        setVisible(!visible)
-    }
+  const toggleVisibility = () =>
+  {
+    setVisible(!visible)
+  }
 
-    useImperativeHandle(ref, () => 
-    {
-        return { toggleVisibility }
-    })
+  useImperativeHandle(ref, () =>
+  {
+    return { toggleVisibility }
+  })
 
-    return (
-        <div>
-        <div style={hideWhenVisible}>
+  return (
+    <div>
+      <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
-        </div>
-        <div style={showWhenVisible}>
+      </div>
+      <div style={showWhenVisible} className="togglableContent">
 
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 })
 
 Togglable.displayName = 'Togglable'
 
-Togglable.propTypes = 
+Togglable.propTypes =
 {
-    buttonLabel: PropTypes.string.isRequired
+  buttonLabel: PropTypes.string.isRequired
 }
 
 export default Togglable
