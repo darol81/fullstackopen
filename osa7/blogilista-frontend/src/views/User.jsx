199102e5bcc9c6c06c0dffe5378a81731f6be7cd
Topi Mansikkaviita <tomansi@gmail.com>
2025-01-06 19:06:28 +0200

    refactoring, const blogs removed, using user.blogs

--- osa7/blogilista-frontend/src/views/User.jsx
+++ osa7/blogilista-frontend/src/views/User.jsx
@@ -10,21 +10,19 @@ const User = () =>
     const userList = useSelector(state => state.user);
     const user = userList.find(u => u.id === userid);
 
-    
     useEffect(() => 
     {
         dispatch(initializeUsers());
     }, [dispatch]);
 
     if(userList === null || user == null) return null;
-    const blogs = user.blogs;
  
     return (
             <div>   
                 <h2>{user.name}</h2>
                 <h3>Added blogs</h3>   
                 <ul>
-                    {blogs.map(blog => (
+                    {user.blogs.map(blog => (
                         <li key={blog.id}>{blog.title}</li>
                     ))} 
                 </ul>
